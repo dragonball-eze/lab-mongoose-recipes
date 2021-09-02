79ffef8596d7758d879f67678572f95061b74f9e
Special K <kelian.somon@gmail.com>
2021-09-02 17:24:53 +0200

    finished

--- index.js
+++ index.js
@@ -1,27 +1,61 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 // Import of the model Recipe from './models/Recipe.model.js'
-const Recipe = require('./models/Recipe.model');
+const Recipe = require("./models/Recipe.model");
 // Import of the data from './data.json'
-const data = require('./data');
+const data = require("./data");
 
-const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
+const MONGODB_URI = "mongodb://localhost:27017/recipe-app";
 
 // Connection to the database "recipe-app"
 mongoose
-  .connect(MONGODB_URI, {
-    useCreateIndex: true,
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-  })
-  .then(self => {
+  .connect(MONGODB_URI)
+  .then((self) => {
     console.log(`Connected to the database: "${self.connection.name}"`);
     // Before adding any recipes to the database, let's remove all existing ones
-    return Recipe.deleteMany()
+    return Recipe.deleteMany();
   })
   .then(() => {
-    // Run your code here, after you have insured that the connection was made
+      updateDB();
   })
-  .catch(error => {
-    console.error('Error connecting to the database', error);
+  .catch((error) => {
+    console.error("Error connecting to the database", error);
   });
+
+
+
+const updateDB = async ()=> {
+  //////////////////////////////////////////
+  //Create a new reciped
+ /* const createdRecipe = await Recipe.create({
+  title: "abc",
+  level: "Easy Peasy",
+  ingredients: ["whatever"],
+  cuisine: "something",
+  dishType: "other",
+  image: "default",
+  duration: 0,
+  creator: "abc",
+});
+
+console.log(createdRecipe.title) */
+try{
+await Recipe.insertMany(data);
+data.forEach((recipe)=> console.log(recipe.title));
+
+const recipeUpdate = await Recipe.findOneAndUpdate({title: "Rigatoni alla Genovese"}, {duration: 100});
+console.log(`${recipeUpdate.title} success`);
+
+await Recipe.deleteOne({title: "Carrot Cake"});
+console.log("recipe deleted");
+
+/////////////////////////////////////
+} catch(e) {
+  console.log("error occured", e); 
+} finally {
+  mongoose.connection.close();
+  console.log("closed go enjoy your date");
+}
+}
+ 
+
